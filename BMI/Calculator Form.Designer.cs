d5ec7d6aa49ba6228c219499e63d6b24b4d354fa
Shujaa Ahmad <shujaa25@gmail.com>
2022-04-05 16:23:54 +0530

    initial commit

---
+++ BMI/Calculator Form.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace BMI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.weightLabel = new System.Windows.Forms.Label();
+            this.heightLabel = new System.Windows.Forms.Label();
+            this.weightBox = new System.Windows.Forms.TextBox();
+            this.heightBox = new System.Windows.Forms.TextBox();
+            this.calculateBtn = new System.Windows.Forms.Button();
+            this.bmiLabel = new System.Windows.Forms.Label();
+            this.bmiValueLabel = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.categoryLabel = new System.Windows.Forms.Label();
+            this.categoryResultLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.wikiLink = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // weightLabel
+            // 
+            this.weightLabel.AutoSize = true;
+            this.weightLabel.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.weightLabel.Location = new System.Drawing.Point(12, 9);
+            this.weightLabel.Name = "weightLabel";
+            this.weightLabel.Size = new System.Drawing.Size(168, 27);
+            this.weightLabel.TabIndex = 0;
+            this.weightLabel.Text = "Weight (kg):";
+            // 
+            // heightLabel
+            // 
+            this.heightLabel.AutoSize = true;
+            this.heightLabel.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.heightLabel.Location = new System.Drawing.Point(12, 45);
+            this.heightLabel.Name = "heightLabel";
+            this.heightLabel.Size = new System.Drawing.Size(168, 27);
+            this.heightLabel.TabIndex = 1;
+            this.heightLabel.Text = "Height (ft):";
+            // 
+            // weightBox
+            // 
+            this.weightBox.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.weightBox.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.weightBox.Location = new System.Drawing.Point(186, 6);
+            this.weightBox.MaxLength = 4;
+            this.weightBox.Name = "weightBox";
+            this.weightBox.Size = new System.Drawing.Size(267, 34);
+            this.weightBox.TabIndex = 2;
+            // 
+            // heightBox
+            // 
+            this.heightBox.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.heightBox.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.heightBox.Location = new System.Drawing.Point(186, 42);
+            this.heightBox.MaxLength = 4;
+            this.heightBox.Name = "heightBox";
+            this.heightBox.Size = new System.Drawing.Size(267, 34);
+            this.heightBox.TabIndex = 3;
+            // 
+            // calculateBtn
+            // 
+            this.calculateBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(67)))), ((int)(((byte)(54)))));
+            this.calculateBtn.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.calculateBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.calculateBtn.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateBtn.ForeColor = System.Drawing.Color.Black;
+            this.calculateBtn.Location = new System.Drawing.Point(186, 82);
+            this.calculateBtn.Name = "calculateBtn";
+            this.calculateBtn.Size = new System.Drawing.Size(267, 45);
+            this.calculateBtn.TabIndex = 4;
+            this.calculateBtn.Text = "Calculate";
+            this.calculateBtn.UseVisualStyleBackColor = false;
+            this.calculateBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // bmiLabel
+            // 
+            this.bmiLabel.AutoSize = true;
+            this.bmiLabel.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bmiLabel.Location = new System.Drawing.Point(12, 173);
+            this.bmiLabel.Name = "bmiLabel";
+            this.bmiLabel.Size = new System.Drawing.Size(142, 27);
+            this.bmiLabel.TabIndex = 5;
+            this.bmiLabel.Text = "BMI Value:";
+            // 
+            // bmiValueLabel
+            // 
+            this.bmiValueLabel.AutoSize = true;
+            this.bmiValueLabel.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bmiValueLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.bmiValueLabel.Location = new System.Drawing.Point(182, 176);
+            this.bmiValueLabel.Name = "bmiValueLabel";
+            this.bmiValueLabel.Size = new System.Drawing.Size(25, 27);
+            this.bmiValueLabel.TabIndex = 6;
+            this.bmiValueLabel.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(133, 574);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(198, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "© Shujaa Ahmad - 2018";
+            // 
+            // categoryLabel
+            // 
+            this.categoryLabel.AutoSize = true;
+            this.categoryLabel.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.categoryLabel.Location = new System.Drawing.Point(12, 210);
+            this.categoryLabel.Name = "categoryLabel";
+            this.categoryLabel.Size = new System.Drawing.Size(142, 27);
+            this.categoryLabel.TabIndex = 10;
+            this.categoryLabel.Text = "Category: ";
+            // 
+            // categoryResultLabel
+            // 
+            this.categoryResultLabel.AutoSize = true;
+            this.categoryResultLabel.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.categoryResultLabel.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.categoryResultLabel.Location = new System.Drawing.Point(182, 210);
+            this.categoryResultLabel.Name = "categoryResultLabel";
+            this.categoryResultLabel.Size = new System.Drawing.Size(25, 27);
+            this.categoryResultLabel.TabIndex = 11;
+            this.categoryResultLabel.Text = "-";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 264);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 23);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Reference (WHO):";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(28, 290);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(408, 263);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // wikiLink
+            // 
+            this.wikiLink.AutoSize = true;
+            this.wikiLink.Location = new System.Drawing.Point(274, 264);
+            this.wikiLink.Name = "wikiLink";
+            this.wikiLink.Size = new System.Drawing.Size(109, 23);
+            this.wikiLink.TabIndex = 14;
+            this.wikiLink.TabStop = true;
+            this.wikiLink.Text = "Wikipedia";
+            this.wikiLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.wikiLink_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(465, 606);
+            this.Controls.Add(this.wikiLink);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.categoryResultLabel);
+            this.Controls.Add(this.categoryLabel);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.bmiValueLabel);
+            this.Controls.Add(this.bmiLabel);
+            this.Controls.Add(this.calculateBtn);
+            this.Controls.Add(this.heightBox);
+            this.Controls.Add(this.weightBox);
+            this.Controls.Add(this.heightLabel);
+            this.Controls.Add(this.weightLabel);
+            this.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BMI Calculator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label weightLabel;
+        private System.Windows.Forms.Label heightLabel;
+        private System.Windows.Forms.TextBox weightBox;
+        private System.Windows.Forms.TextBox heightBox;
+        private System.Windows.Forms.Button calculateBtn;
+        private System.Windows.Forms.Label bmiLabel;
+        private System.Windows.Forms.Label bmiValueLabel;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label categoryLabel;
+        private System.Windows.Forms.Label categoryResultLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.LinkLabel wikiLink;
+    }
+}
+
---
+++ BMI/Calculator Form.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try {
+                double weight = double.Parse(weightBox.Text);
+                double height = double.Parse(heightBox.Text);
+                double heightInM = height / 3.2808;
+                double bmi = weight / (heightInM * heightInM);
+                bmi = Math.Round(bmi, 2);
+
+                String resultMessage = "";
+
+                if (bmi < 16)
+                {
+                    resultMessage = "Underweight (Severe thinness)";
+                }
+                else if (bmi <= 16.9)
+                {
+                    resultMessage = "Underweight (Moderate thinness)";
+                }
+                else if (bmi <= 18.4)
+                {
+                    resultMessage = "Underweight (Mild thinness)";
+                }
+                else if (bmi <= 24.9)
+                {
+                    resultMessage = "Normal range";
+                }
+                else if (bmi <= 29.9)
+                {
+                    resultMessage = "Overweight (Pre-obese)";
+                }
+                else if (bmi <= 34.9)
+                {
+                    resultMessage = "Obese (Class I)";
+                }
+                else if (bmi <= 39.9)
+                {
+                    resultMessage = "Obese (Class II)";
+                }
+                else if (bmi >= 40) {
+                    resultMessage = "Obese (Class III)";
+                }
+
+                bmiValueLabel.Text = bmi.ToString() + " kg/m^2";
+                categoryResultLabel.Text = resultMessage;
+            }
+            catch (Exception) {
+                MessageBox.Show("An error occured!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void wikiLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://en.wikipedia.org/wiki/Body_mass_index");
+        }
+    }
+}
